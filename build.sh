8db5cab09ea0f56381144ddc92e3a2f4164cf843
Lukas-JavaScript <205667645+codedbylukas@users.noreply.github.com>
2026-03-30 09:03:57 -0400

    fixed commit

--- build.sh
+++ build.sh
@@ -7,3 +7,13 @@ dotnet publish -r osx-arm64 --self-contained true -p:PublishSingleFile=true -p:F
 dotnet publish -r win-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:FixedNames=true
 dotnet publish -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:FixedNames=true
 dotnet publish -r osx-arm64 -c Release --self-contained true -p:PublishSingleFile=true -p:FixedNames=true
+
+# Debug Builds
+rsync -av --exclude="*.pdb" ./bin/Debug/net6.0/linux-x64/publish/ ./binarys/debug/linux-x64/
+rsync -av --exclude="*.pdb" ./bin/Debug/net6.0/osx-arm64/publish/ ./binarys/debug/osx-arm64/
+rsync -av --exclude="*.pdb" ./bin/Debug/net6.0/win-x64/publish/ ./binarys/debug/win-x64/
+
+# Release Builds
+rsync -av --exclude="*.pdb" ./bin/Release/net6.0/linux-x64/publish/ ./binarys/release/linux-x64/
+rsync -av --exclude="*.pdb" ./bin/Release/net6.0/osx-arm64/publish/ ./binarys/release/osx-arm64/
+rsync -av --exclude="*.pdb" ./bin/Release/net6.0/win-x64/publish/ ./binarys/release/win-x64/
