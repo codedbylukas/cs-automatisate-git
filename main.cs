0b00bb9776ff9161cf96cbe9e4b9ba18dae5959b
codedbylukas <lukaswittek@gmail.com>
2026-05-16 06:26:15 -0500

    add

---
+++ main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommandRunner;
+using initConfig;
+using Commits;
+using Push;
+
+
+public static class Program
+{
+    public static void Main()
+    {
+        init.initFn();
+    bool pushing = push.pushFn();
+
+    while (commit.commitFn(pushing))
+    {
+
+    }
+    }
+}
+
