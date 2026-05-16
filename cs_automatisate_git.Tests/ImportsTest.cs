846454a6448974c86768720512902c8ad501068e
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-05-16 08:08:36 -0500

    add

---
+++ cs_automatisate_git.Tests/ImportsTest.cs
@@ -0,0 +1,17 @@
+using CommandRunner;
+using BranchMode;
+using Commits;
+using initConfig;
+using cs_automatisate_git;
+using Push;
+using CommandRunner;
+
+public class ImportsTest
+{
+    [Fact]
+    public void TestImports()
+    {
+        // This test will fail if any of the imports are incorrect or if there are missing dependencies.
+        Assert.True(true);
+    }
+}
