a39dc4fb1cde38054d6ff3a61e4ab60837cd15db
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-05-17 04:26:51 -0500

    add

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@ set -e
 
 os_list=(
     "win-x64"
+    "win-x86"
+    "win-arm64"
     "linux-x64"
+    "linux-arm64"
+    "linux-arm"
+    "linux-musl-x64"
+    "linux-musl-arm64"
+    "osx-x64"
     "osx-arm64"
 )
 
